6369e6cd7348ebade5d91f8a325670a66243d34d
hiennguyen-pc <nminhhien99@gmail.com>
2021-04-18 19:25:43 +0700

    initial commit

---
+++ ChatFirebase/app/src/main/java/com/example/chatfirebase/DangKi.java
@@ -0,0 +1,14 @@
+package com.example.chatfirebase;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DangKi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dang_ki);
+    }
+}
